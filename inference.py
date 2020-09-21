eb7107a9b8178aa7110755af4420137f0a60090f
Lukas Rauh <lukas.rauh@r-a-u-h.de>
2020-09-21 13:30:51 +0200

    added workspace changes in inference class

--- inference.py
+++ inference.py
@@ -36,32 +36,114 @@ class Network:
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
+        """
+        Initialize Network object variables with None
+        """
+        self.plugin = None
+        self.network = None
+        self.input_blob = None
+        self.output_blob = None
+        self.exec_network = None
+        self.infer_request = None
 
-    def load_model(self):
+    def load_model(self, model_xml, device="CPU", cpu_extension=None):
+        """
+        Load the model given IR files on the device and add extensions
+        :param model_xml: the path to the model XML file
+        :param device: the desired device for the inference, CPU is default
+        :param cpu_extension: the cpu extension file, if CPU is selected as device
+        """
         ### TODO: Load the model ###
         ### TODO: Check for supported layers ###
         ### TODO: Add any necessary extensions ###
         ### TODO: Return the loaded inference plugin ###
         ### Note: You may need to update the function parameters. ###
+        
+        # Create model binary file path and check existence
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+        if not os.path.exists(model_bin):
+            sys.exit("ERROR: path \"{}\" is not a valid file path for the model binaries!".format(model_bin))
+
+        # Initialize the plugin
+        self.plugin = IECore()
+
+        # Add a CPU extension, if applicable
+        if cpu_extension and "CPU" in device:
+            self.plugin.add_extension(cpu_extension, device)
+
+        # Read the IR as a IENetwork
+        self.network = IENetwork(model=model_xml, weights=model_bin)
+        
+        # Get the supported layers of the network
+        supported_layers = self.plugin.query_network(network=self.network, device_name=device)
+
+        # Check for any unsupported layers. Exit the program, if so.
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            log.warn("Unsupported layers found: {}".format(unsupported_layers))
+            log.warn("Check whether extensions are available to add to IECore.")
+            sys.exit("ERROR: unsupported layers found!")
+
+        # Load the IENetwork into the plugin
+        self.exec_network = self.plugin.load_network(self.network, device)
+
+        # Get the input layer
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+        
+        log.info("IR model \"{}\" succesfully loaded!".format(os.path.basename(model_xml)))
+        
         return
 
+    
     def get_input_shape(self):
+        """
+        Returns the input shape of the network
+        :return: shape of the input
+        """
         ### TODO: Return the shape of the input layer ###
-        return
+        return self.network.inputs[self.input_blob].shape
+    
+    def get_output_shape(self):
+        """
+        Returns the output shape of the network
+        :return: shape of the output
+        """
+        ### TODO: Return the shape of the input layer ###
+        return self.network.outputs[self.output_blob].shape
 
-    def exec_net(self):
+    def exec_net(self, image, request_id=0):
+        """
+        Starts an asynchronous inference request
+        :param image: the input image
+        :param request_id: an optional request id that is given in the request, default is "0"
+        """
         ### TODO: Start an asynchronous request ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
+        self.infer_request = self.exec_network.start_async(request_id=request_id, inputs={self.input_blob: image})
         return
+    
 
-    def wait(self):
+    def wait(self, request_id=0):
+        """
+        Waits until the inference request for the given request_id is finished
+        :param request_id: the request id that is given in the request, default is "0"
+        :return: the status of the processed request
+        """
         ### TODO: Wait for the request to be complete. ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
-
-    def get_output(self):
+        status = self.exec_network.requests[request_id].wait(-1)
+        return status
+        
+        
+    def get_output(self, request_id=0):
+        """
+        Returns the extracted output for the given request_id
+        :param request_id: the request id that is given in the request, default is "0"
+        :return: the extracted output for the given request_id
+        """
         ### TODO: Extract and return the output results
         ### Note: You may need to update the function parameters. ###
-        return
+        return self.exec_network.requests[request_id].outputs[self.output_blob]
